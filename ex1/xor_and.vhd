91f2a01e4a6bb303046956cfcfd893349a22ca2a
Panagiotis Spentzaris <73785089+Pspetz@users.noreply.github.com>
2022-04-02 02:21:36 +0300

    Create xor_and.vhd

---
+++ ex1/xor_and.vhd
@@ -0,0 +1,20 @@
+----------------------------------------------------
+LIBRARY ieee;
+USE ieee.std_logic_1164.all;
+----------------------------------------------------
+ENTITY xor_with_and IS
+
+	PORT (inputA, inputB : IN STD_LOGIC_VECTOR(7 DOWNTO 0);
+	      out_and : OUT STD_LOGIC_VECTOR(7 DOWNTO 0);
+	      out_xor : OUT STD_LOGIC_VECTOR(7 DOWNTO 0));
+		  
+END and_xor;
+----------------------------------------------------
+ARCHITECTURE schematic OF xor_with_and IS
+
+BEGIN
+	out_and <= inputA AND inputB;
+	out_xor <= inputA XOR inputB;
+	
+END schematic ;
+--------------------------------------------------
